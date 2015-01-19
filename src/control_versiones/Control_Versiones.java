708bd783caef577712de65f5b3c6e602f3ac5739
Patricia <Patricia@Patri>
2015-01-19 09:27:04 +0100

    Inicio experimentos

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -7,6 +7,7 @@ public class Control_Versiones {
     
     public static void main(String[] args) {
         System.out.println("Probando checkout revision");
+        System.out.println("Inicio de la branch experimentos");
     }
     
 }
