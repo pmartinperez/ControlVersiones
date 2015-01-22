6adfe7a34bb3dc5185bfcac399d4d1b48ba62d74
Patricia <Patricia@Patri>
2015-01-22 09:17:28 +0100

    Cambios en la clase experimental

--- src/control_versiones/Experimental.java
+++ src/control_versiones/Experimental.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package control_versiones;
 
-/**
- *
- * @author Patricia
- */
+
 public class Experimental {
-    
+   //Esta clase la dejaremos para probar cosas nuevas
 }
