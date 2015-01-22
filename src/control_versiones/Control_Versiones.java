1437df2973ae3ceb32fe3b702b523c08f0967abc
Patricia <Patricia@Patri>
2015-01-22 09:19:27 +0100

    Merge con la rama experimental

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -9,6 +9,7 @@ public class Control_Versiones {
         System.out.println("Probando checkout revision");
         System.out.println("Inicio de la branch experimentos");
         System.out.println("Se ha creado una clase nueva en la rama experimental donde probaremos nuestras cosillas");
+        System.out.println("Se ha hecho un merge con la rama experimental");
     }
     
 }
