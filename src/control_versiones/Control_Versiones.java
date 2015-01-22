d06612e6ac8c36fcc06dff902e74206215396c3f
Patricia <Patricia@Patri>
2015-01-22 09:15:14 +0100

    informacion sobre la clase experimental

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -8,6 +8,7 @@ public class Control_Versiones {
     public static void main(String[] args) {
         System.out.println("Probando checkout revision");
         System.out.println("Inicio de la branch experimentos");
+        System.out.println("Se ha creado una clase nueva en la rama experimental donde probaremos nuestras cosillas");
     }
     
 }
