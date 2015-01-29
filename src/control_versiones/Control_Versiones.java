15a314d6250d49ec2d675dddf7b8f98ac2f2a175
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-29 09:14:24 +0100

    Modificacion Fran Muestra variables y suma+i Scanner Añadido

--- src/control_versiones/Control_Versiones.java
+++ src/control_versiones/Control_Versiones.java
@@ -1,6 +1,8 @@
 
 package control_versiones;
 
+import java.util.Scanner;
+
 
 public class Control_Versiones {
 
@@ -8,8 +10,10 @@ public class Control_Versiones {
     public static void main(String[] args) {
         int suma=0;
         System.out.println("Probando checkout revision");
-        System.out.println("Inicio de la branch experimentos");
-        System.out.println("Se ha creado una clase nueva en la rama experimental donde probaremos nuestras cosillas");
+        System.out.println("Mete dato");
+        Scanner dato=new Scanner (System.in);
+        suma=dato.nextInt();
+        
         System.out.println("Se ha hecho un merge con la rama experimental");
         
         System.out.println("Nueva rama EXPERIMENTO2");
